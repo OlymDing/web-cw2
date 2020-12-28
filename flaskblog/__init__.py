aee04708ac34afb85741302c8f14b1b13678ce10
Olym <1013752750@qq.com>
2020-12-28 10:52:24 +0800

    finish initializing logger, can be called at other times

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -4,7 +4,8 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
 from flaskblog.config import Config
-
+import logging
+from flask.logging import default_handler
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
@@ -14,9 +15,11 @@ login_manager.login_message_category = 'info'
 mail = Mail()
 
 
+# create the flask app
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(Config)
+    init_log(app)
 
     db.init_app(app)
     bcrypt.init_app(app)
@@ -33,3 +36,13 @@ def create_app(config_class=Config):
     app.register_blueprint(errors)
 
     return app
+
+# for initializing app's logger
+def init_log(app):
+    app.logger.setLevel(logging.DEBUG)
+    file_handler = logging.FileHandler('./flaskblog/static/record.log')
+    file_handler.setLevel(logging.INFO)
+
+    file_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    file_handler.setFormatter(file_formatter)
+    app.logger.addHandler(file_handler)
