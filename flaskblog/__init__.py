b9a2e8455f4b2bc3b8da25744bb65f647169803d
Olym <1013752750@qq.com>
2020-12-29 14:09:41 +0800

    change __init__ structure

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -7,7 +7,6 @@ from flaskblog.config import Config
 import logging
 from flask.logging import default_handler
 
-db = SQLAlchemy()
 bcrypt = Bcrypt()
 login_manager = LoginManager()
 login_manager.login_view = 'users.login'
@@ -15,34 +14,27 @@ login_manager.login_message_category = 'info'
 mail = Mail()
 
 
-# create the flask app
-def create_app(config_class=Config):
-    app = Flask(__name__)
-    app.config.from_object(Config)
-    init_log(app)
+app = Flask(__name__)
+app.config.from_object(Config)
 
-    db.init_app(app)
-    bcrypt.init_app(app)
-    login_manager.init_app(app)
-    mail.init_app(app)
+app.logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler('./flaskblog/static/record.log')
+file_handler.setLevel(logging.INFO)
 
-    from flaskblog.users.routes import users
-    from flaskblog.posts.routes import posts
-    from flaskblog.main.routes import main
-    from flaskblog.errors.handlers import errors
-    app.register_blueprint(users)
-    app.register_blueprint(posts)
-    app.register_blueprint(main)
-    app.register_blueprint(errors)
+file_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(file_formatter)
+app.logger.addHandler(file_handler)
 
-    return app
+db = SQLAlchemy(app)
+bcrypt.init_app(app)
+login_manager.init_app(app)
+mail.init_app(app)
 
-# for initializing app's logger
-def init_log(app):
-    app.logger.setLevel(logging.DEBUG)
-    file_handler = logging.FileHandler('./flaskblog/static/record.log')
-    file_handler.setLevel(logging.INFO)
-
-    file_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-    file_handler.setFormatter(file_formatter)
-    app.logger.addHandler(file_handler)
+from flaskblog.users.routes import users
+from flaskblog.posts.routes import posts
+from flaskblog.main.routes import main
+from flaskblog.errors.handlers import errors
+app.register_blueprint(users)
+app.register_blueprint(posts)
+app.register_blueprint(main)
+app.register_blueprint(errors)
