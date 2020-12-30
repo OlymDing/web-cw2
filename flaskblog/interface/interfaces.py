83a742efaae45e37fc5e790a43875397d4248468
Olym <1013752750@qq.com>
2020-12-30 23:44:45 +0800

    the first like click instance, what a day ...

--- flaskblog/interface/interfaces.py
+++ flaskblog/interface/interfaces.py
@@ -10,8 +10,7 @@ from flask import Blueprint
 
 interfaces = Blueprint('interfaces', __name__)
 
-class Test(Resource):
-    def get(self):
-        return {'wow': 'testing'}
-
-api.add_resource(Test, '/test')
+@interfaces.route("/like/<int:post_id>", methods=["POST"])
+def test(post_id):
+    print(post_id)
+    return {'test': 'wow'}
