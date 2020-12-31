3c8bf51f7a2bf4857b874eb0175d7ee10766c2af
Olym <1013752750@qq.com>
2020-12-31 22:37:06 +0800

    encountered with the unlogged problem, trying to fix it out

--- flaskblog/interface/interfaces.py
+++ flaskblog/interface/interfaces.py
@@ -1,10 +1,13 @@
 from flaskblog import api
+from flask_login import current_user, login_required
 from flask_restful import Resource
 from flask import Blueprint
+from flaskblog.models import Like
 
 interfaces = Blueprint('interfaces', __name__)
 
 @interfaces.route("/like/<int:post_id>", methods=["POST"])
+@login_required
 def like(post_id):
-    print(post_id)
-    return {'test': 'wow'}
+    print(current_user.id)
+    return str(3)
