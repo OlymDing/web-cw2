7db3424cf934546705e2fc98b27c1ab319a24b3b
Olym <1013752750@qq.com>
2021-01-01 22:48:25 +0800

    comments 50% finished, now able to display comments and add new ones, next to add validators, should user wtforms perhaps

--- flaskblog/interface/interfaces.py
+++ flaskblog/interface/interfaces.py
@@ -1,8 +1,8 @@
 from flaskblog import api, db
 from flask_login import current_user, login_required
 from flask_restful import Resource
-from flask import Blueprint, flash, jsonify
-from flaskblog.models import Like, Post
+from flask import Blueprint, flash, jsonify, request
+from flaskblog.models import Like, Post, Comment
 from sqlalchemy.exc import DatabaseError
 
 interfaces = Blueprint('interfaces', __name__)
@@ -30,3 +30,20 @@ def like(post_id):
             return jsonify({'founded': False, 'count':count})
     else:
         return jsonify({'founded': False})
+
+@interfaces.route("/make_comment/<int:post_id>", methods=["POST"])
+def make_comment(post_id):
+    if current_user.is_authenticated:
+        comment_str = request.form.get('comment')
+        new_comment = Comment(content = comment_str, user_id = current_user.id, post_id = post_id)
+        db.session.add(new_comment)
+        db.session.commit()
+    return jsonify({'content': new_comment.content, 'time': new_comment.date_commented, 'user_id': new_comment.user_id })
+
+@interfaces.route("/get_comments/<int:post_id>")
+def get_comments(post_id):
+    comments = Comment.query.filter_by(post_id = post_id).all()
+    comment_list = []
+    for i in comments:
+        comment_list.append({'content': i.content, 'time': i.date_commented, 'user_id': i.user_id })
+    return jsonify({'data': comment_list})
