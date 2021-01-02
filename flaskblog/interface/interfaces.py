69e6af2d081cd5d854e436632bc3a93f7d1e96c4
Olym <1013752750@qq.com>
2021-01-02 23:02:17 +0800

    finish the comment name and imgs

--- flaskblog/interface/interfaces.py
+++ flaskblog/interface/interfaces.py
@@ -1,7 +1,7 @@
 from flaskblog import api, db
 from flask_login import current_user, login_required
 from flask_restful import Resource
-from flask import Blueprint, flash, jsonify, request
+from flask import Blueprint, flash, jsonify, request, redirect, url_for
 from flaskblog.models import Like, Post, Comment
 from sqlalchemy.exc import DatabaseError
 
@@ -35,15 +35,19 @@ def like(post_id):
 def make_comment(post_id):
     if current_user.is_authenticated:
         comment_str = request.form.get('comment')
-        new_comment = Comment(content = comment_str, user_id = current_user.id, post_id = post_id)
+        new_comment = Comment(content = comment_str, author = current_user, post_id = post_id)
         db.session.add(new_comment)
         db.session.commit()
-    return jsonify({'content': new_comment.content, 'time': new_comment.date_commented.strftime("%Y %m %d %H:%M "), 'user_id': new_comment.user_id })
+    return jsonify({'content': new_comment.content, 'time': new_comment.date_commented.strftime("%Y %m %d %H:%M "), 'username': new_comment.author.username, 'img': new_comment.author.image_file })
 
 @interfaces.route("/get_comments/<int:post_id>")
 def get_comments(post_id):
     comments = Comment.query.filter_by(post_id = post_id).all()
     comment_list = []
     for i in comments:
-        comment_list.append({'content': i.content, 'time': i.date_commented.strftime("%Y %m %d %H:%M "), 'user_id': i.user_id })
+        comment_list.append({'content': i.content, 'time': i.date_commented.strftime("%Y %m %d %H:%M "), 'username': i.author.username, 'img': i.author.image_file })
     return jsonify({'data': comment_list})
+
+@interfaces.route("/get_image")
+def img():
+    return redirect(url_for('static', filename="profile_pics/default.jpg"))
